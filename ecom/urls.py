3029bf6a77dfb717be222b203af49059ee6c6e96
tanvirislamchayan <imdtanivr181@gmail.com>
2024-04-04 13:01:54 +0600

    setup

--- ecom/urls.py
+++ ecom/urls.py
@@ -6,8 +6,12 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('product.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
+#     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+
+# <img src="{% static 'images/example.jpg' %}" alt="Example Image">
+
+# <img src="{{ instance.image.url }}" alt="Image">
--- product/views.py
+++ product/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    page = 'Home'
+    context = {
+        'page': page,
+    }
+    return render(request, 'home/home.html', context=context)
+
+
+def product(request):
+    page = 'Shop'
+    context = {
+        'page': page,
+    }
+    return render(request, 'product/product.html', context=context)
